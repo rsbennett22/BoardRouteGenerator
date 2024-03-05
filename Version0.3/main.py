dde9323dd48bd1165b7079a097fafdb2afd7d4fb
Rafe <rafebennett22@gmail.com>
2024-03-05 01:13:47 +0000

    * Update main checklist

--- Version0.3/main.py
+++ Version0.3/main.py
@@ -5,9 +5,10 @@
 
 '''
 TODO:
-    - Create a way to plot the holds on a wall programmatically
-    - Create the next iteration of a hold implementation from scratch handling the above
-    - Implement next iteration of route generation from scratch implementing above
+    - Create a way to plot the holds on a wall programmatically - done
+    - Create the next iteration of the Hold class - done as part of above
+        - include hold difficulty rating
+    - Implement next iteration of route generation from scratch
         - need methods for generating the next hold
         - ability to choose amount of holds on the route
         - this is all to be done in the main method
