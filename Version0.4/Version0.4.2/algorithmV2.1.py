b1abb30f54d65ca2bc37903be45e942ea32f34e1
Rafe <rafebennett22@gmail.com>
2024-03-29 22:15:06 +0000

    * Add variation option to route generation if start and finish in a straight line

---
+++ Version0.4/Version0.4.2/algorithmV2.1.py
@@ -0,0 +1,152 @@
+### Rafe Bennett 25/04/2024
+
+'''
+TODO:
+    - Want to be able to generate route by hold type(s)
+        - Want it to work with just one hold type first, start with jugs as easiest to see if working correctly,
+          then add pinches as next easiest to verify before unleashing full hold selection option
+
+        - Need to modify method that chooses a random hold to instead choose a random hold that is the hold type wanted
+            - Generate a list of holds, find all the holds of hold type x, randomly choose from this list of holds
+            - If can't choose a hold then change where the midpoint is
+'''
+
+
+
+import logging
+import RouteHelper
+import GlobalHelper
+import matplotlib.pyplot as plt
+from time import sleep
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+GENERATED_ROUTE = []
+holds = None
+startHolds = []
+finishHolds = []
+startHold = None
+finishHold = None
+tree = None
+holdPoints = None
+WIDEN_ROUTE = False
+MAX_DIST = 230
+
+
+def setup():
+    global GENERATED_ROUTE
+    global holds
+    global startHolds
+    global finishHolds
+    global startHold
+    global finishHold
+    global tree
+    global holdPoints
+    global WIDEN_ROUTE
+    GlobalHelper.applyImageToPlt()
+    holds = GlobalHelper.getHoldsFromFile()
+    treeAndHoldPoints = RouteHelper.generateCkdTreeWithHoldPoints(holds)
+    tree = treeAndHoldPoints[0]
+    holdPoints = treeAndHoldPoints[1]
+    startHolds = RouteHelper.defineStartHolds(holds)
+    finishHolds = RouteHelper.defineFinishHolds(holds)
+    startHold = RouteHelper.randomlySelectHoldFromList(startHolds)
+    finishHold = RouteHelper.randomlySelectHoldFromList(finishHolds)
+    GENERATED_ROUTE.append(startHold)
+    GENERATED_ROUTE.append(finishHold)
+
+
+def algorithm(numOfHolds):
+    global GENERATED_ROUTE
+    global tree
+    global holdPoints
+    global finishHolds
+
+    currentHold = 0
+    midHold = None
+
+    # Check if 0, 1 or 2 holds chosen
+    lessThanTwoHoldsWanted = RouteHelper.checkIfLessThanTwoHoldsWanted(numOfHolds)
+
+    if (lessThanTwoHoldsWanted):
+        if numOfHolds == 0:
+            # Empty board
+            GENERATED_ROUTE = []
+        if numOfHolds == 1:
+            # One hold wanted
+            GENERATED_ROUTE = [GENERATED_ROUTE[0]]
+
+    # Increase current hold by 2 as start hold and finish hold been generated already
+    currentHold = 2
+    while currentHold < numOfHolds and not lessThanTwoHoldsWanted:
+        
+        # Find the hold with the largest distance to the next hold in list
+        largestDistHolds = RouteHelper.findLargestDistanceBetweenHolds(GENERATED_ROUTE)
+        hold1 = largestDistHolds[0]
+        hold2 = largestDistHolds[1]
+        hold1Pos = largestDistHolds[2]
+
+        # Generate the midHold and insert into route
+        midHold = RouteHelper.createMidHold(hold1, hold2, holds, tree, holdPoints, finishHolds, GENERATED_ROUTE)
+        GENERATED_ROUTE.insert(hold1Pos+1, midHold)
+        
+        currentHold += 1
+    
+
+
+def runProgram():
+    global GENERATED_ROUTE
+    global holds
+
+    setup()
+    numOfHolds = GlobalHelper.askUserForNumOfHolds()
+    algorithm(numOfHolds)
+    GlobalHelper.plotHolds(GENERATED_ROUTE, "b")
+    plt.show()
+
+
+runProgram()
+
+
+def testAlgorithmGeneratesExpectedLengthRoutes():
+    # Generate 10 routes for each route length 0 - 15, verify the route length is correct
+    global GENERATED_ROUTE
+    numFailed = 0
+    numPassed = 0
+
+    for i in range(15):
+        for y in range(50):
+            setup()
+            # Generate 10 routes per route length i
+            algorithm(i)
+            if (len(GENERATED_ROUTE) != i):
+                numFailed += 1
+            else:
+                numPassed += 1
+            GENERATED_ROUTE = []
+    print("FAILED: " + str(numFailed) + "\nPASSED: " + str(numPassed))
+
+#testAlgorithmGeneratesExpectedLengthRoutes()
+
+
+def testMidholdVariation():
+    global startHolds
+    global finishHolds
+    global startHold
+    global finishHold
+    global GENERATED_ROUTE
+
+    numOfHolds = GlobalHelper.askUserForNumOfHolds()
+    numOfRoutes = int(input("Number of routes to generate: "))
+    for i in range(numOfRoutes):
+        setup()
+        GENERATED_ROUTE[0] = startHolds[15]
+        GENERATED_ROUTE[1] = finishHolds[7]
+        algorithm(numOfHolds)
+        GlobalHelper.plotHolds(GENERATED_ROUTE, "b")
+        plt.pause(10)
+        GENERATED_ROUTE = []
+        plt.close('all')
+
+#testMidholdVariation()
